2862fb30a0c6fdbd60adc273a7c2241852fac090
Marc Botifoll <mbotifoll@icn2.net>
2023-06-26 19:10:17 +0200

    Removed nextnanopy master

--- FEM_modelling_code/nextnanopy-master/nextnanopy/__init__.py
+++
@@ -1,9 +0,0 @@
-from nextnanopy.outputs import DataFile, DataFolder
-from nextnanopy.commands import execute
-from nextnanopy.inputs import InputFile, Sweep
-from nextnanopy.defaults import NNConfig
-# from nextnanopy.shapes import GdsPolygonsRaw
-
-config = NNConfig()
-
-
